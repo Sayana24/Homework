25bf3433441a09d34e3d7cbf734fc12bac90bec2
Sayana <s@mail.ru>
2024-06-10 01:00:18 +0700

    + src/homework8/Program.java in Homework

--- src/homework8/Program.java
+++ src/homework8/Program.java
@@ -1,2 +1,12 @@
-package homework8;public class Program {
+package homework8;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число, оканчивающееся на 5. Возведем его в квадрат");
+        long n = sc.nextInt();
+        System.out.println((n/10 * (n/10 + 1)) * 100 + 25);
+    }
 }
